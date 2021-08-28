0e3348dc2234535456cafa9405330113da4cfdd8
M F M Fazrin <mfmfazrin1986@gmail.com>
2021-08-28 12:18:58 +0530

    using Microsoft.AspNetCore.Mvc; using Microsoft.Extensions.Logging; using System.Diagnostics; using WeatherAPI.Models;

---
+++ Models/Weather.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Weather.Data
+{
+    public class Weather
+    {
+        [Key] public Guid Id { get; set; }
+        public int Humidity { get; set; }
+        public int Temperature { get; set; }
+        public int Min_Temperature { get; set; }
+        public int Max_Temperature { get; set; }
+    }
+}
