218062ef7dc1fd50cc36bbd667a8e601f13a0e19
M F M Fazrin <mfmfazrin1986@gmail.com>
2021-08-28 11:50:49 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WeatherAPI.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
